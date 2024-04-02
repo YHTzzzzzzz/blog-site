11b881d52be2687dc18b86508daed5711757e424
YHTzzzzzzz <1448637274@qq.com>
2024-04-02 22:58:43 +0800

    fix: init project

---
+++ src/main/java/cn/htcode/blogsite/BlogSiteApplication.java
@@ -0,0 +1,16 @@
+package cn.htcode.blogsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Yin
+ */
+@SpringBootApplication
+public class BlogSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogSiteApplication.class, args);
+    }
+
+}
